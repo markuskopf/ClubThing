9a6e04905f0339325630ab27efe6e8e63dcdf1dc
Markus Kopf <kopf.markus@googlemail.com>
2013-10-26 13:25:39 +0200

    Initial Commit

---
+++ TrojanThing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TrojanThing
+//
+//  Created by Markus Kopf on 26/10/13.
+//  Copyright (c) 2013 Markus Kopf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TrojanThing/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TrojanThing
+//
+//  Created by Markus Kopf on 26/10/13.
+//  Copyright (c) 2013 Markus Kopf. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
